7fefae661bab3c7830301445ee4ff808cc99b56b
Karina Valens <karyvalens@yahoo.com>
2023-01-24 15:32:38 +0100

    projects animations

--- components/content/Projects.js
+++ components/content/Projects.js
@@ -1,30 +1,65 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useContext } from "react";
-import { AppContext } from "../context/AppContext";
 import { projects } from "../../pages/api/projects";
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 export default function Projects() {
+  const [container, setContainer] = useState();
+
+  useMemo(() => {
+    const containerValue = {
+      hidden: { opacity: 0, scale: 0 },
+      visible: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+          delayChildren: 1,
+          staggerChildren: 0.2,
+        },
+      },
+    };
+    return setContainer(containerValue);
+  }, []);
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
+  const projectArt = useRef(null);
+  const projectIsInView = useInView(projectArt, { threshold: 1 });
+  const projectAnim = useAnimation();
+
+  useEffect(() => {
+    console.log("element is", projectIsInView);
+    if (projectIsInView) {
+      projectAnim.start({ container });
+    }
+  }, [projectIsInView, projectAnim, container]);
+
   return (
     <>
-      <section className="projects">
+      <motion.article className="projects" ref={projectArt}>
         <h2 className="sub-title ">Projects</h2>
-        <div className="grid">
+        <motion.div className="grid" variants={projectAnim} initial="hidden" animate="visible">
           {projects.map((project) => {
             return (
-              <div key={project.id} className="center flex glass-effect">
+              <motion.div key={project.id} className="center flex glass-effect" variants={item}>
                 <div className="picture-container">
                   <Image src={project.projectimg} alt={project.name} fill sizes="(max-width: 700px) 100vw, 700px" />
                 </div>
                 <Link className="glass-effect link" href={`projects/${project.name}`}>
                   {project.title}
                 </Link>
-              </div>
+              </motion.div>
             );
           })}
-        </div>
-      </section>
+        </motion.div>
+      </motion.article>
     </>
   );
 }
